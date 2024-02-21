a134f83255be92ffdc28cb3ab82f7ad5677d93e4
lgeec2740 <dmitriymachtak@gmail.com>
2024-02-21 21:38:01 +1000

    Добавьте файлы проекта.

---
+++ WebApplication1Product/DB/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1Product.DB;
+
+public partial class Status
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+}
---
+++ WebApplication1Product/User11Context.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1Product.DB;
+
+namespace WebApplication1Product;
+
+public partial class User11Context : DbContext
+{
+    public User11Context()
+    {
+    }
+
+    public User11Context(DbContextOptions<User11Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Status> Statuses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=192.168.200.35;database=user11;user=user11;password=59674;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("Product");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Description).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(50);
+
+            entity.HasOne(d => d.IdStatusNavigation).WithMany(p => p.Products)
+                .HasForeignKey(d => d.IdStatus)
+                .HasConstraintName("FK_Product_Status");
+        });
+
+        modelBuilder.Entity<Status>(entity =>
+        {
+            entity.ToTable("Status");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
